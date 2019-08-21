e42e43c93f9390b92542925bfd9cc05088596da8
Adrian Cueto <adrianjpca@gmail.com>
2019-08-21 18:08:47 -0500

    The way the data is processed is changed, because the fields vary depending on the configuration of the gateways.

--- uplink.py
+++ uplink.py
@@ -4,10 +4,10 @@ import json
 import openpyxl
 import base64
 from base64 import b64decode
-global iRow, iColDev, iColMeta, iColGw, nGw;
+global iRow, nGw;
 
-app_id = "beacon-encontrack"
-access_key = "ttn-account-v2.czFauryQ1RGYZTkuneK-GtFj_C3o38EsHvbW062SC1o"
+app_id = "beacons"
+access_key = "ttn-account-v2.ErABTkHwaF7QCVuIY0ScHh9ezPnMCaUnwhcD8DqPIo8"
 dest_filename = "LoRa.xlsx"  #The name of File Excel
 
 doc = openpyxl.load_workbook(filename=dest_filename) #Open File Excel
@@ -16,29 +16,37 @@ iRow=3 #begin of row third
 
 #================= Function Callback ===========================================
 def uplink_callback(msg, client):
-    global iRow, iColMeta, iColGw, nGw; #variable definition
-
+    global iRow, nGw; #variable definition
     print("Received uplink from ", msg.dev_id)
-
+    iColGw=14
     nGw = len(msg[6][6]) # gateways number
-    iColDev=1; iColMet=7; iColGw=14
     #===== General processing =================================================
-    gSheet.cell(row=iRow, column=1).value = msg.dev_id
-    gSheet.cell(row=iRow, column=2).value = msg.hardware_serial
-    gSheet.cell(row=iRow, column=3).value = msg.port
-    gSheet.cell(row=iRow, column=4).value = msg.counter
-    gSheet.cell(row=iRow, column=5).value = b64decode(msg.payload_raw).hex() #convert payload from base64 to hexadecimal
+    gSheet.cell(row=iRow, column=1).value = msg.app_id
+    gSheet.cell(row=iRow, column=2).value = msg.dev_id
+    gSheet.cell(row=iRow, column=3).value = msg.hardware_serial
+    gSheet.cell(row=iRow, column=4).value = msg.port
+    gSheet.cell(row=iRow, column=5).value = msg.counter
+    gSheet.cell(row=iRow, column=6).value = b64decode(msg.payload_raw).hex() #convert payload from base64 to hexadecimal
     #====== Metadata processing ===============================================
-    for iVal in range(6):
-        gSheet.cell(row=iRow, column=iColMet).value = msg.metadata[iVal]
-       iColMet+=1 #column increment
+    gSheet.cell(row=iRow, column=7).value = msg.metadata.time
+    gSheet.cell(row=iRow, column=8).value = msg.metadata.frequency
+    gSheet.cell(row=iRow, column=9).value = msg.metadata.modulation
+    gSheet.cell(row=iRow, column=10).value = msg.metadata.data_rate
+    gSheet.cell(row=iRow, column=11).value = msg.metadata.airtime
+    gSheet.cell(row=iRow, column=12).value = msg.metadata.coding_rate
+    gSheet.cell(row=iRow, column=13).value = nGw # gateways number
     #====== Gateways processing ================================================
     for iGw in range(nGw):
-        for gVal in range(6):
-            gSheet.cell(row=iRow, column=iColGw).value = msg.metadata.gateways[iGw][gVal]
-            iColGw+=1;#column increment
+        gSheet.cell(row=iRow, column=iColGw+0).value = msg.metadata.gateways[iGw].gtw_id
+        gSheet.cell(row=iRow, column=iColGw+1).value = msg.metadata.gateways[iGw].timestamp
+        gSheet.cell(row=iRow, column=iColGw+2).value = msg.metadata.gateways[iGw].time
+        gSheet.cell(row=iRow, column=iColGw+3).value = msg.metadata.gateways[iGw].channel
+        gSheet.cell(row=iRow, column=iColGw+4).value = msg.metadata.gateways[iGw].rssi
+        gSheet.cell(row=iRow, column=iColGw+5).value = msg.metadata.gateways[iGw].snr
+        gSheet.cell(row=iRow, column=iColGw+6).value = msg.metadata.gateways[iGw].latitude
+        gSheet.cell(row=iRow, column=iColGw+7).value = msg.metadata.gateways[iGw].longitude
+        iColGw+=8; #column increment
 
-    gSheet.cell(row=iRow, column=13).value = nGw # print gateway number
     doc.save(filename = dest_filename) #save file
     print("File updated")
     iRow+=1 #row increment
